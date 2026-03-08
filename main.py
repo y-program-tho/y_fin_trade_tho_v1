71dfe020287b63bbbd6d28f47ba7b5271665051d
y-program-tho <yanga12edu@gmail.com>
2026-03-08 17:26:25 +0200

    etl yfinance data into google sheets and read data from it

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import etl.yfinance_etl_to_gs as yf_to_gs
 
 st.title("y_fin_trade_tho_v1")
-stonks_data = yf_to_gs.proto_stonks_data()
+stonks_data = yf_to_gs.get_stonk_data_from_gs()
 st.dataframe(stonks_data)
 
 # Spacing
@@ -13,5 +13,6 @@ st.write(" ")
 ticker = st.selectbox("What stock would you like to view",
                      ('MSFT', 'AAPL', 'GOOG', "NVDA", "AMZN")
                      )
+st.write(f"The columns are {[stonks_data.columns]}")
 st.subheader(f"{ticker} Stock Chart")
-st.line_chart(stonks_data[stonks_data['Ticker'] == ticker]['Close'], y='Close')
+st.line_chart(stonks_data[stonks_data['Ticker'] == ticker], x='Date', y='Close')
