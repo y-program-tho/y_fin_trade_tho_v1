23708aed4dfbed7cd7edf78d8e6f989732980dab
y-program-tho <yanga12edu@gmail.com>
2026-03-17 17:31:58 +0200

    create google sheet service class

---
+++ services/sheet_services.py
@@ -0,0 +1,43 @@
+import os
+import pandas as pd
+import gspread
+from dotenv import load_dotenv, dotenv_values 
+from google.oauth2.service_account import Credentials
+
+
+class SheetService:
+    def __init__(self, credentials, sheet_name):
+
+        load_dotenv()
+        scope = os.getenv('SCOPE')
+        cred_file = os.getenv('CRED_FILE')
+        sheet_id = os.getenv('SHEET_ID')
+
+        creds = Credentials.from_service_account_file(cred_file, scopes=[scope])
+
+        client = gspread.authorize(creds)
+        self.sheet = client.open_by_key(sheet_id)
+
+    def read_sheet(self, worksheet_name):
+        worksheet = self.sheet.worksheet(worksheet_name)
+        return pd.DataFrame(worksheet.get_all_records())
+
+    def write_to_sheet(self, worksheet_name, df):
+
+        worksheet = self.sheet.worksheet(worksheet_name)
+
+        worksheet.clear()
+
+        worksheet.update(
+            [
+                df.columns.values.tolist() +
+                df.values.tolist()
+            ]
+        )
+
+    def append_rows_to_sheet(self, worksheet_name, df):
+
+        worksheet = self.sheet.worksheet(worksheet_name)
+
+        worksheet.append_rows(df.values.tolist())
+
