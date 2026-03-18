ee64e9795c8ce0a6c5d4562da7b20f7aed513cdd
y-program-tho <yanga12edu@gmail.com>
2026-03-18 17:56:11 +0200

    refactor etl and sheet services

--- etl/yfinance_etl_to_gs.py
+++ etl/yfinance_etl_to_gs.py
@@ -1,26 +1,10 @@
-import os
 import pandas as pd
 import yfinance as yf
-import streamlit as st
-import gspread
-from dotenv import load_dotenv, dotenv_values
-from google.oauth2.service_account import Credentials
+from services.sheet_services import SheetService
 
-def gs_config():
-    load_dotenv()
-    scope = os.getenv('SCOPE')
-    cred_file = os.getenv('CRED_FILE')
-    sheet_id = os.getenv('SHEET_ID')
-    creds = Credentials.from_service_account_file(cred_file, scopes=[scope])
-    client = gspread.authorize(creds)
-    gs_workbook = client.open_by_key(sheet_id)
-    return gs_workbook
-
-# gs_workbook.worksheet.sheet1.get_all_values()
-
-def yf_stocks_data_to_gs(stonk_list=['MSFT', 'AAPL', 'GOOG', "NVDA", "AMZN"], period='1mo'):
+def run_yf_to_gs_etl():
     # Dowload data from yfinance for top 5 stocks
-    stonk_data = yf.download(stonk_list, period=period)
+    stonk_data = yf.download(['MSFT', 'AAPL', 'GOOG', "NVDA", "AMZN"], period='1mo')
 
     # Transform stock data dataframe from wide to long 
     stonk_df_step_one = stonk_data.stack()
@@ -29,17 +13,6 @@ def yf_stocks_data_to_gs(stonk_list=['MSFT', 'AAPL', 'GOOG', "NVDA", "AMZN"], pe
     stonk_df_final['Date'] = stonk_df_final['Date'].astype(str)
 
     # Load data into google sheets
-    stonk_gs_wb = gs_config()
-    stonk_gs_wb.sheet1.update([stonk_df_final.columns.values.tolist()] + stonk_df_final.values.tolist())
+    sheet_service = SheetService()
+    sheet_service.write_to_sheet("stock_price", stonk_df_final)
     return f"Data for {period} has been loaded to Google Sheets successfully. Size of data:{stonk_df_final.shape}"
-
-@st.cache_data(ttl=1800)
-def get_stonk_data_from_gs():
-    stonk_gs_wb = gs_config()
-    stonks_df = pd.DataFrame(stonk_gs_wb.sheet1.get_all_values())
-    stonks_df.columns = stonks_df.iloc[0]
-    stonks_df = stonks_df[1:]
-    stonks_df = stonks_df.reset_index(drop=True)
-    stonks_df = stonks_df.rename_axis(None, axis=1)
-    stonks_df['Date'] = pd.to_datetime(stonks_df['Date'])
-    return stonks_df
--- services/sheet_services.py
+++ services/sheet_services.py
@@ -1,12 +1,13 @@
 import os
 import pandas as pd
+import streamlit as st
 import gspread
 from dotenv import load_dotenv, dotenv_values 
 from google.oauth2.service_account import Credentials
 
 
 class SheetService:
-    def __init__(self, credentials, sheet_name):
+    def __init__(self):
 
         load_dotenv()
         scope = os.getenv('SCOPE')
@@ -18,9 +19,17 @@ class SheetService:
         client = gspread.authorize(creds)
         self.sheet = client.open_by_key(sheet_id)
 
+    @st.cache_data(ttl=1800)
     def read_sheet(self, worksheet_name):
         worksheet = self.sheet.worksheet(worksheet_name)
-        return pd.DataFrame(worksheet.get_all_records())
+        stonks_df = pd.DataFrame(worksheet.get_all_records())
+        stonks_df.columns = stonks_df.iloc[0]
+        stonks_df = stonks_df[1:]
+        stonks_df = stonks_df.reset_index(drop=True)
+        stonks_df = stonks_df.rename_axis(None, axis=1)
+        stonks_df['Date'] = pd.to_datetime(stonks_df['Date'])
+        return stonks_df
+
 
     def write_to_sheet(self, worksheet_name, df):
 
