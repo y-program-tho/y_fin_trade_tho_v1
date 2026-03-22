64167ef594c4bf42e62cbcb81ea0cff751b1f8f3
y-program-tho <yanga12edu@gmail.com>
2026-03-22 17:45:12 +0200

    Add detections and more validations

--- etl/yfinance_to_gs_etl.py
+++ etl/yfinance_to_gs_etl.py
@@ -2,12 +2,6 @@ import pandas as pd
 import yfinance as yf
 from services.sheet_services import SheetService
 
-def extract_stock_data_yfinance():
-
-    stonks_df = yf.download(['MSFT', 'AAPL', 'GOOG', "NVDA", "AMZN"], period='1mo')
-
-    return stonks_df
-
 
 def clean_stock_data(df):
 
@@ -19,17 +13,46 @@ def clean_stock_data(df):
     
     return df 
 
+
+def transform_yf_multi_stock_data(df):
+
+    df = df.stack()
+    df = df.rename_axis(None, axis=1)
+    df = df.reset_index()
+    df['Date'] = df['Date'].astype(str)
+
+    return df  
+
 def run_yf_to_gs_etl_multi_stock():
     # Dowload data from yfinance for top 5 stocks
-    stonks_data = extract_stock_data_yfinance()
+    stonks_df = yf.download(['MSFT', 'AAPL', 'GOOG', "NVDA", "AMZN"], period='1mo')
+
+    stonks_df = clean_stock_data(stonks_df)
 
     # Transform stock data dataframe from wide to long 
-    stonk_df_step_one = stonk_data.stack()
-    stonk_df_step_two = stonk_df_step_one.rename_axis(None, axis=1)
-    stonk_df_final = stonk_df_step_two.reset_index()
-    stonk_df_final['Date'] = stonk_df_final['Date'].astype(str)
+    stonks_df = transform_yf_multi_stock_data(stonks_df)
 
     # Load data into google sheets
     sheet_service = SheetService()
     sheet_service.write_to_sheet("stock_price", stonk_df_final)
     return f"Data for {period} has been loaded to Google Sheets successfully. Size of data:{stonk_df_final.shape}"
+
+def validate_stock_data(df):
+
+    if df['Close'].isnull().any():
+        raise ValueError("Missing close prices")
+
+    if (df['Close'] < 0).any():
+        raise ValueError("Negative price detected")
+
+    return True
+
+def detect_dupes_or_outliers(df):
+
+    if len(df.duplicated()) > 0:
+        print("Duplicates detected")
+
+    if df["Close"].max()>1000:
+        print("Possible bad data detected")
+
+    return df
