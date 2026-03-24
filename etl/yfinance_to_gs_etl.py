6c210deaf1b3d124e9ed2732a323c40e62303cb8
y-program-tho <yanga12edu@gmail.com>
2026-03-24 17:54:59 +0200

    update etl process

--- etl/yfinance_to_gs_etl.py
+++ etl/yfinance_to_gs_etl.py
@@ -1,38 +1,44 @@
+import logging
 import pandas as pd
 import yfinance as yf
 from services.sheet_services import SheetService
 
+logger = logging.getLogger(__name__)
+
 def clean_stock_data(df):
 
-    logger.info(f"")
-
+    logger.info(f"Cleaning data from yfinance")
+    
     df.columns = df.columns.str.lower()
-
+    
     df = df.fillna()
     
     df = df.drop_duplicates()
+    
+    df['date'] = df['date'].astype(str)
     
     return df 
 
 def transform_yf_multi_stock_data(df):
 
-    logger.info(f"")
+    logger.info(f"Changing the structure of table")
 
     df = df.stack()
+    
     df = df.rename_axis(None, axis=1)
+    
     df = df.reset_index()
-    df['Date'] = df['Date'].astype(str)
-
+    
     return df  
 
 def validate_stock_data(df):
 
     logger.info(f"")
 
-    if df['Close'].isnull().any():
+    if df['close'].isnull().any():
         raise ValueError("Missing close prices")
 
-    if (df['Close'] < 0).any():
+    if (df['close'] < 0).any():
         raise ValueError("Negative price detected")
 
     return True
@@ -44,7 +50,7 @@ def detect_dupes_or_outliers(df):
     if len(df.duplicated()) > 0:
         print("Duplicates detected")
 
-    if df["Close"].max()>1000:
+    if df["close"].max()>1000:
         print("Possible bad data detected")
 
     return
@@ -56,13 +62,13 @@ def run_yf_to_gs_etl_multi_stock():
     # Dowload data from yfinance for top 5 stocks
     stonks_df = yf.download(['MSFT', 'AAPL', 'GOOG', "NVDA", "AMZN"], period='1mo')
 
-    stonks_df = clean_stock_data(stonks_df)
+    sheet_service = SheetService()
+    sheet_service.write_to_sheet("raw_stock_data", stonks_df)
 
     # Transform stock data dataframe from wide to long 
     stonks_df = transform_yf_multi_stock_data(stonks_df)
 
-    # Load data into google sheets
-    sheet_service = SheetService()
-    sheet_service.write_to_sheet("stock_price", stonk_df_final)
-    return f"Data for {period} has been loaded to Google Sheets successfully. Size of data:{stonk_df_final.shape}"
+    stonks_df = clean_stock_data(stonks_df)
 
+    sheet_service.write_to_sheet("processed_stock_data", stonk_df_final)
+    return f"Data for {period} has been loaded to Google Sheets successfully. Size of data:{stonk_df_final.shape}"
