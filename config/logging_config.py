023f5977644bb5b2f423d25ebcce6f89d534aa25
y-program-tho <yanga12edu@gmail.com>
2026-04-15 19:23:55 +0200

    refactor code to comment out logging till functional

--- config/logging_config.py
+++ config/logging_config.py
@@ -3,6 +3,6 @@ import logging
 def setup_logging():
 
     logging.basicConfig(
-        level=logging.Info,
-        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+        level=logging.info,
+        format="%(asctime)s  %(levelname)s  %(name)s  %(message)s"
     )
